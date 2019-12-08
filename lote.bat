cfc97a99a6ad9aa158423cb0e4e208e83d1c68ba
rodriguez <grodriguezfies@unprg.edu.pe>
2019-12-08 15:55:47 -0500

    Condicional Simple 02

--- lote.bat
+++ lote.bat
@@ -1,3 +1,7 @@
 python rodriguez/simple01.py "Cristina", 16
 python rodriguez/simple01.py "Patricia", 12
 python rodriguez/simple01.py "Susana", 10
+
+python rodriguez/simple02.py "Jose", 200
+python rodriguez/simple02.py "Lidia", 160
+python rodriguez/simple02.py "Juana", 155
