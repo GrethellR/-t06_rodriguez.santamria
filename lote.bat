50b7ddbaa3367d0d739644506e67b4355c063af1
GrethellR <58376183+grethellr@users.noreply.github.com>
2019-12-08 15:48:06 -0500

    Condicional Simple 01

---
+++ lote.bat
@@ -0,0 +1,3 @@
+python rodriguez/simple01.py "Cristina", 16
+python rodriguez/simple01.py "Patricia", 12
+python rodriguez/simple01.py "Susana", 10
---
+++ rodriguez/simple01.py
@@ -0,0 +1,18 @@
+# Programa 01
+
+import os
+
+#Declaracion
+nombre,edad=" ",0
+
+#INPUT
+nombre=str(os.sys.argv[1])
+edad=int(os.sys.argv[2])
+
+#PROCESSING
+#Si el usuario es menor de edad no podra entrar al cine
+menor_edad=(edad < 18)
+
+if (edad < 18):
+    print(nombre , "¡Hasta la proxima!")
+
